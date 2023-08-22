10054d731471f178ffc8a1fe47fa8fdf7f6b4581
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-22 03:18:57 +0000

    edited the code block to handle the operations in task 0 of the printf project

--- 0-print_format.c
+++ 0-print_format.c
@@ -4,39 +4,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-char *intToBinaryString(int num)
-{
-	int temp = num;
-	int binaryLength = 0;
-	int index = binaryLength - 1;
-	char *binaryStr = (char *)malloc(binaryLength + 1);
-
-	if (num == 0)
-	{
-		return strdup("0");
-	}
-
-	while (temp > 0)
-	{
-		temp /= 2;
-		binaryLength++;
-	}
-
-	if (binaryStr == NULL)
-	{
-		return NULL;
-	}
-
-	while (num > 0)
-	{
-		binaryStr[index] = (num % 2) + '0';
-		num /= 2;
-		index--;
-	}
-	binaryStr[binaryLength] = '\0';
-	return (binaryStr);
-}
-
 /**
  * _printf - Function to print arguments in their given formats
  * @format: the input argument format
@@ -46,63 +13,38 @@ char *intToBinaryString(int num)
 
 int _printf(const char *format, ...)
 {
-	int i, count = 0, j, k;
-	char secondarg;
-	char *thirdarg, str[20], *binaryStr;
-
+	int count = 0, secondarg;
+	char *thirdarg;
+	const char *ptr;
 	va_list(result);
 	va_start(result, format);
 
-	for (i = 0; format[i]; i++)
+	for (ptr = format; *ptr != '\0'; ptr++)
 	{
-		if (format[i] != '%')
+		if (*ptr != '%')
 		{
-			putchar(format[i]);
+			putchar(*ptr);
 			count++;
 		}
 		else
 		{
-			i++;
-			if (format[i] == '%')
+			ptr++;
+			if (*ptr == '%')
 			{
 				putchar('%');
 				count++;
 			}
-			else if (format[i] == 'c')
+			else if (*ptr == 'c')
 			{
 				secondarg = va_arg(result, int);
 				putchar(secondarg);
 				count++;
 			}
-			else if (format[i] == 's')
+			else if (*ptr == 's')
 			{
 				thirdarg = va_arg(result, char*);
 				fputs(thirdarg, stdout);
 				count += strlen(thirdarg);
-			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				j = va_arg(result, int);
-				sprintf(str, "%d", j);
-				fputs(str, stdout);
-				count += strlen(str);
-			}
-			else if (format[i] == 'b')
-			{
-				k = va_arg(result, int);
-				binaryStr = intToBinaryString(k);
-				if (binaryStr != NULL)
-				{
-					fputs(binaryStr, stdout);
-					count += strlen(binaryStr);
-					free(binaryStr);
-				}
-			}
-			else
-			{
-				putchar('%');
-				putchar(format[i]);
-				count += 2;
 			}
 		}
 	}
