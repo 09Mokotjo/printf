ad1e1f16bd4b242918929668532f44147167bfe3
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-23 07:04:28 +0000

    resolved the git conflict and merged all files correctly

--- 0-print_format.c
+++ 0-print_format.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-#include <unistd.h>
+
 /**
  * _printf - Function to print arguments in their given formats
  * @format: the input argument format
@@ -10,57 +10,6 @@
  * Return: Always 0 success
  */
 
-<<<<<<< HEAD
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int count = 0;
-    const char *current = format;
-
-    while (*current != '\0') {
-        if (*current == '%') {
-            current++;
-            switch (*current) {
-                case 'c': {
-                    char c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char *);
-                    while (*s != '\0') {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    // Handles the unsupported specifier by ignoring it
-                    break;
-            }
-        } else {
-            putchar(*current);
-            count++;
-        }
-        current++;
-    }
-
-    va_end(args);
-    return count;
-}
-
-int main() {
-    _printf("Hello, %s! The character is %c and a %% sign.\n", "world", 'A');
-    return 0;
-=======
 int _printf(const char *format, ...)
 {
 	int count, secondarg, j;
@@ -119,7 +68,6 @@ int _printf(const char *format, ...)
 	}
 	va_end(result);
 	return (count);
->>>>>>> 1291157b5a8337f15a33f291a9ef5325fdc2991f
 }
 
 void handle_numeric(va_list result, int *count, char specifier)
